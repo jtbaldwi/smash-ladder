45c25e70c72a1080fb84725ef49a8108cef2236a
Justin Baldwin <jtbaldwi@gmail.com>
2019-06-29 15:26:50 -0400

    Added PlayerService logic

--- src/main/groovy/com/smash/ladder/services/PlayerService.groovy
+++ src/main/groovy/com/smash/ladder/services/PlayerService.groovy
@@ -1,9 +1,36 @@
 package com.smash.ladder.services
 
+import com.smash.ladder.data.Player
 import groovy.transform.CompileStatic
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
 @CompileStatic
 class PlayerService {
+
+    static ResponseEntity createPlayer(Player player) {
+        new ResponseEntity(HttpStatus.OK)
+    }
+
+    static ResponseEntity getPlayer(String id) {
+        new ResponseEntity(HttpStatus.OK)
+    }
+
+    static ResponseEntity updatePlayer(Player player) {
+        new ResponseEntity(HttpStatus.OK)
+    }
+
+    static ResponseEntity deletePlayer(String id) {
+        new ResponseEntity(HttpStatus.OK)
+    }
+
+    static ResponseEntity updatePlayerWinLoss(String id) {
+        new ResponseEntity(HttpStatus.OK)
+    }
+
+    static ResponseEntity updatePlayerElo(String id) {
+        new ResponseEntity(HttpStatus.OK)
+    }
 }
